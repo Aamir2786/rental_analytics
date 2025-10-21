14b8bb764eb843e347fd657268a2e53b32722b98
Aamir <aamirsohel96@gmail.com>
2025-10-21 16:24:12 +0530

    handle correct end of consecutive stay (#6)

--- models/marts/rental_analysis/mart_listing_consecutive_stays.sql
+++ models/marts/rental_analysis/mart_listing_consecutive_stays.sql
@@ -5,7 +5,10 @@ with consecutive_stays as (
 select
     listing_id,
     group_start_date as start_of_consecutive_stay,
-    group_end_date as end_of_consecutive_stay,
+    dateadd(day, 
+        least(consecutive_nights_based_on_availability, maximum_nights_allowed) - 1, 
+        group_start_date
+    ) as end_of_consecutive_stay,
     least(consecutive_nights_based_on_availability, maximum_nights_allowed) as max_night_stay_possible
 from
     consecutive_stays
